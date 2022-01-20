c7a302920f8d7a83ab909c8f134f294bc50fb537
Johannes ter Haak <johannes@terhaak.de>
2022-01-20 22:27:42 +0100

    asm code can now be embedded into the binary

--- main.go
+++ main.go
@@ -4,13 +4,50 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"regexp"
+	"strconv"
 
 	"terhaak.de/imp/pkg/asm"
+	"terhaak.de/imp/pkg/vm"
 )
 
+func parseMemFlags() vm.Program {
+	var prog vm.Program
+	if len(os.Args) == 1 {
+		return prog
+	}
+	r := regexp.MustCompile("^([0-9]+):(.*)$")
+	for _, f := range os.Args[1:] {
+		if m := r.FindStringSubmatch(f); m != nil {
+			addr, _ := strconv.ParseInt(m[1], 10, 0)
+			value, _ := strconv.ParseInt(m[2], 10, 0)
+			prog = append(prog, vm.PushInt(value), vm.StoreMemory(addr))
+		}
+	}
+	return prog
+}
+
+func execEmbedded() {
+	if prog, err := asm.LoadEmbeddedAssembly(); err != nil {
+		fmt.Printf("Error: %v\n", err)
+		os.Exit(2)
+	} else if prog != nil {
+		prog = append(parseMemFlags(), prog...)
+		if err := vm.RunProgram(prog); err != nil {
+			fmt.Printf("Error: %v\n", err)
+			os.Exit(2)
+		}
+		os.Exit(0)
+	}
+
+}
+
 func main() {
+	execEmbedded()
+
 	asmCmd := flag.NewFlagSet("asm", flag.ExitOnError)
 	asmFile := asmCmd.String("f", "", "Path to the asm file to run")
+	asmOutFile := asmCmd.String("embed", "", "Path to new file to create with VM and embedded code")
 
 	switch os.Args[1] {
 	case "asm":
@@ -21,7 +58,13 @@ func main() {
 	}
 
 	if asmCmd.Parsed() {
-		err := asm.RunAssemblyFile(*asmFile)
+		var err error
+		if *asmFile != "" && *asmOutFile == "" {
+			err = asm.RunAssemblyFile(*asmFile)
+		} else if *asmFile != "" && *asmOutFile != "" {
+			err = asm.EmbedAssemblyFile(*asmOutFile, *asmFile)
+		}
+
 		if err != nil {
 			fmt.Printf("Error: %v\n", err)
 		}
--- pkg/asm/asm.go
+++ pkg/asm/asm.go
@@ -2,7 +2,11 @@ package asm
 
 import (
 	"bufio"
+	"bytes"
+	"encoding/binary"
 	"fmt"
+	"io"
+	"io/ioutil"
 	"os"
 	"regexp"
 	"strconv"
@@ -68,7 +72,7 @@ func ParseMnemonic(s string) (vm.Executer, error) {
 
 }
 
-func ParseAssemblyFile(file *os.File) (vm.Program, error) {
+func ParseAssemblyFile(file io.Reader) (vm.Program, error) {
 	var program vm.Program
 	scanner := bufio.NewScanner(file)
 	scanner.Split(bufio.ScanLines)
@@ -112,4 +116,86 @@ func DumpAssemblyProgram(prog vm.Program) {
 	for _, inst := range prog {
 		fmt.Printf("%v\n", inst)
 	}
+}
+
+const magicString = "embeddedcodecode"
+
+func LoadEmbeddedAssembly() (vm.Program, error) {
+	exeFile, err := os.Open("/proc/self/exe")
+	if err != nil {
+		return nil, err
+	}
+	defer exeFile.Close()
+
+	// [binary:var][asm_text:var][asm_size:8Byte][magic:fixed]
+	suffixSize := int64(len(magicString) + 8)
+
+	if _, err := exeFile.Seek(-1*suffixSize, io.SeekEnd); err != nil {
+		return nil, err
+	}
+
+	var codeSize int64
+	if suffix, err := ioutil.ReadAll(exeFile); err != nil {
+		return nil, err
+	} else if !bytes.Equal(suffix[8:], []byte(magicString)) {
+		// no embedded program
+		return nil, nil
+	} else {
+		// network byte order
+		codeSize = int64(binary.BigEndian.Uint64(suffix[:8]))
+	}
+
+	if _, err := exeFile.Seek(-1*(codeSize+suffixSize), io.SeekEnd); err != nil {
+		return nil, err
+	}
+
+	if code, err := ioutil.ReadAll(exeFile); err != nil {
+		return nil, err
+	} else {
+		codeReader := bytes.NewReader(code[:len(code)-int(suffixSize)])
+		return ParseAssemblyFile(codeReader)
+	}
+}
+
+func EmbedAssembly(targetFile io.Writer, sourceFile io.Reader) error {
+	exeFile, err := os.Open("/proc/self/exe")
+	if err != nil {
+		return err
+	}
+	defer exeFile.Close()
+
+	// copy ELF go binary file first
+	if _, err = io.Copy(targetFile, exeFile); err != nil {
+		return err
+	}
+	exeFile.Seek(0, io.SeekEnd)
+
+	// append the source file
+	codeSize, err := io.Copy(targetFile, sourceFile)
+	if err != nil {
+		return err
+	}
+
+	// write size of code in network byte order and fixed size
+	if err := binary.Write(targetFile, binary.BigEndian, uint64(codeSize)); err != nil {
+		return err
+	}
+	// write magic string to detect embedded code
+	if _, err := targetFile.Write([]byte(magicString)); err != nil {
+		return err
+	}
+	return nil
+}
+
+func EmbedAssemblyFile(target, source string) error {
+	binTarget, err := os.Create(target)
+	if err == nil {
+		defer binTarget.Close()
+		binSource, err := os.Open(source)
+		if err == nil {
+			defer binSource.Close()
+			return EmbedAssembly(binTarget, binSource)
+		}
+	}
+	return err
 }
